675459c7faa34b8051df991e0d9f22355ecca402
Jackieee08 <bc.jacqueline.avendano@cvsu.edu.ph>
2024-05-22 15:27:39 +0800

    Update g-arat.sql

--- g-arat.sql
+++ g-arat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2024 at 07:28 PM
+-- Generation Time: May 22, 2024 at 09:24 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,6 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `event-id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `price` decimal(11,0) NOT NULL,
+  `addtl_info` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registrations`
+--
+
+CREATE TABLE `registrations` (
+  `reg-id` int(11) NOT NULL,
+  `user-id` int(11) NOT NULL,
+  `event-id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `reg_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `payment_mode` varchar(100) NOT NULL,
+  `payment_status` varchar(100) NOT NULL,
+  `addtl_data` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -33,12 +67,35 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `studentNum` int(11) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `role` varchar(100) NOT NULL
+  `role` varchar(100) NOT NULL DEFAULT 'User'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user-id`, `fullname`, `email`, `studentNum`, `password`, `role`) VALUES
+(1, 'admin', 'admin@email.com', 245245, '$2y$10$GBVjt27rSqldANCqQl/2Zecs7zHrD9MLBZz5.LMRXGSiEr007Yu4y', 'Admin'),
+(2, 'user', 'user@email.com', 123456, '$2y$10$N.SOxT4ePz2uyFfhEBGPGuRkkYNruJoQhfP49kb8K2J7qe1WQg7wC', 'User'),
+(3, '', '', 0, '$2y$10$XHgHHUnaEvW0Waa0rICUle0VM8jJ83q8jVBcUM0Zpss2CUDQw54dS', 'User'),
+(4, '', '', 0, '$2y$10$GO67n8awrm3kXMWg94fwruMfGyP9eFl2JgVzNDA7HNpE2ql3rhDxe', 'User'),
+(5, '', '', 0, '$2y$10$IIypEK1E3I/hMYaWsTiPn.04hYi1wEMR/NySRTgXPci/qZD9jHpaa', 'User');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`event-id`);
+
+--
+-- Indexes for table `registrations`
+--
+ALTER TABLE `registrations`
+  ADD PRIMARY KEY (`reg-id`);
 
 --
 -- Indexes for table `users`
@@ -51,10 +108,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `event-id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `registrations`
+--
+ALTER TABLE `registrations`
+  MODIFY `reg-id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user-id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user-id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
