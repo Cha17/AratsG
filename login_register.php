04bb2cf49dd0d4652119659c89748b9f411d6822
Charl Cortez <62463803+Cha17@users.noreply.github.com>
2024-05-24 18:09:32 +0800

    format changed

--- login_register.php
+++ login_register.php
@@ -4,27 +4,25 @@ require('conn.php');
 session_start();
 
 //for login
-if(isset($_POST['login'])){
+if (isset($_POST['login'])) {
     $query = "SELECT * FROM users WHERE email = '$_POST[email_username]' OR studentNum = '$_POST[email_username]'";
     $result = mysqli_query($conn, $query);
 
-    if($result){
-        if(mysqli_num_rows($result)==1){
+    if ($result) {
+        if (mysqli_num_rows($result) == 1) {
             $result_fetch = mysqli_fetch_assoc($result);
-            if(password_verify($_POST['password'], $result_fetch['password'])){
+            if (password_verify($_POST['password'], $result_fetch['password'])) {
                 //identify user role
-                if($result_fetch['role']=="User"){
-                    $_SESSION['logged_in']=true;
-                    $_SESSION['email']=$result_fetch['email'];
+                if ($result_fetch['role'] == "User") {
+                    $_SESSION['logged_in'] = true;
+                    $_SESSION['email'] = $result_fetch['email'];
                     header("location: index.php");;
-                }
-                else{
-                    $_SESSION['logged_in']=true;
-                    $_SESSION['email']=$result_fetch['email'];
+                } else {
+                    $_SESSION['logged_in'] = true;
+                    $_SESSION['email'] = $result_fetch['email'];
                     header("location: adminDashboard.php");;
                 }
-            }
-            else{
+            } else {
                 echo "
                 <script>
                     alert('Wrong username or password!');
@@ -32,8 +30,7 @@ if(isset($_POST['login'])){
                 </script>
                 ";
             }
-
-        }else{
+        } else {
             echo "
             <script>
                 alert('Email not registered!');
@@ -41,9 +38,7 @@ if(isset($_POST['login'])){
             </script>
             ";
         }
-
-    }
-    else{
+    } else {
         echo "
         <script>
             alert('Email not registered!');
@@ -51,52 +46,49 @@ if(isset($_POST['login'])){
         </script>
         ";
     }
-
 }
 
 
 //for registration
-if(isset($_POST['signup'])){
+if (isset($_POST['signup'])) {
     $user_exists_query = "SELECT * FROM users WHERE studentNum = '$_POST[studentNum]' OR email = '$_POST[email]'";
     $result = mysqli_query($conn, $user_exists_query);
 }
 
-if($result){
+if ($result) {
     // will be executed if student number or email is already registered
-    if(mysqli_num_rows($result)>0){
+    if (mysqli_num_rows($result) > 0) {
         $result_fetch = mysqli_fetch_assoc($result);
-        if($result_fetch['studentNum'] == $_POST['studentNum']){
+        if ($result_fetch['studentNum'] == $_POST['studentNum']) {
             //if student number already registered
-                echo "
+            echo "
                 <script>
                     alert('Student number already exists! Please make sure to enter your unique student number correctly, or login if you already have an account.');
                     window.location.href = 'login.php';
                 </script>
                 ";
-            }else{
+        } else {
             //if email already registered
-                echo "
+            echo "
                 <script>
                     alert('Email already exists!');
                     window.location.href = 'login.php';
                 </script>
                 ";
-            
-            }
+        }
     }
     // will be executed if student number and email is not yet registered
-    else{
+    else {
         $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
         $query = "INSERT INTO users (fullname, studentNum, email, password) VALUES ('$_POST[username]', '$_POST[studentNum]', '$_POST[email]', '$password')";
-        if(mysqli_query($conn, $query)){
+        if (mysqli_query($conn, $query)) {
             echo "
             <script>
                 alert('Registration successful!');
                 window.location.href = 'login.php';
             </script>
             ";
-        }
-        else{
+        } else {
             // if data cannot be 
             echo "
             <script>
@@ -104,9 +96,9 @@ if($result){
                 window.location.href = 'login.php';
             </script>
             ";
+        }
     }
-    }
-}else{
+} else {
     echo "
     <script>
         alert('Cannot run query!');
@@ -114,6 +106,3 @@ if($result){
     </script>
     ";
 }
-
-
-?>
